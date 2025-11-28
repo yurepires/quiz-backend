68ca687e34076386522075adbd316e032a83cf08
yurepires <contato.yurepires@gmail.com>
2025-11-28 01:48:07 -0300

    feat: responder quiz

--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express'
-import { criar, listarTodos, listarLiberados, buscarPorId, liberar ,responder } from '../controllers/quizController.js'
+import { listarTodos, listarLiberados, buscarPorId, responder } from '../controllers/quizController.js'
 
 const router = express.Router()
 
-router.post('/criarQuiz', criar)                         // POST http://localhost:5000/api/quizzes/criarQuiz
-router.get('/listarQuizzes', listarTodos)                // GET  http://localhost:5000/api/quizzes/listarQuizzes
-router.get('/listarQuizzesLiberados', listarLiberados)   // GET  http://localhost:5000/api/quizzes/listarQuizzesLiberados
-router.get('/buscarQuiz/:id', buscarPorId)               // GET  http://localhost:5000/api/quizzes/buscarQuiz/{ID}
-router.patch('/liberarQuiz/:id', liberar)                // PATCH http://localhost:5000/api/quizzes/liberar/{ID}
-router.post('/responder/:id', responder)                 // POST http://localhost:5000/api/v1/quizzes/responder/{ID_QUIZ}
+router.get('/', listarTodos)                   // GET http://localhost:5000/api/quizzes/
+router.get('/liberados', listarLiberados)      // GET http://localhost:5000/api/quizzes/liberados
+router.get('/:id', buscarPorId)                // GET http://localhost:5000/api/quizzes/{ID}
+router.post('/responder/:id', responder)       // POST http://localhost:5000/api/quizzes/responder/{ID}
 
 export default router
